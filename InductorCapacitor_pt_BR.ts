90cff30eac3e585dc4ca5c61be9af5ac7cd2c960
maico-smaniotto <maicosmaniotto@gmail.com>
2020-08-10 03:27:20 -0300

    Included missing default translation file

---
+++ InductorCapacitor_pt_BR.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="InductorCapacitor_pt_BR">
+<context>
+    <name>AboutWindow</name>
+    <message>
+        <location filename="aboutwindow.ui" line="14"/>
+        <source>Sobre...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutwindow.ui" line="49"/>
+        <source>Calculadora de Indutância e Capacitância</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutwindow.ui" line="71"/>
+        <source>Criador:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutwindow.ui" line="103"/>
+        <source>Contato:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutwindow.ui" line="149"/>
+        <source>Projeto:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutwindow.ui" line="189"/>
+        <source>Fechar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Calculadora de Indutância e Capacitância</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="34"/>
+        <source>Indutor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="53"/>
+        <location filename="mainwindow.ui" line="372"/>
+        <source>Frequência</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="182"/>
+        <location filename="mainwindow.ui" line="261"/>
+        <source>Alternar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="205"/>
+        <location filename="mainwindow.ui" line="208"/>
+        <location filename="mainwindow.ui" line="343"/>
+        <location filename="mainwindow.ui" line="346"/>
+        <source>Copiar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="248"/>
+        <source>Capacitor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="136"/>
+        <location filename="mainwindow.cpp" line="147"/>
+        <location filename="mainwindow.cpp" line="158"/>
+        <location filename="mainwindow.cpp" line="169"/>
+        <source>Impedância</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="139"/>
+        <location filename="mainwindow.cpp" line="144"/>
+        <source>Indutância</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="161"/>
+        <location filename="mainwindow.cpp" line="166"/>
+        <source>Capacitância</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
